04b26398d98887aa36c490e4e6b3d89ecc72a509
sandboxexperimentation <101840766+sandboxexperimentation@users.noreply.github.com>
2022-03-17 15:54:21 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,62 @@
+const PancrelipaseInfo = {
+    "Creon 3000": {
+        "lipase": 3000,
+        "protease": 9500,
+        "amylase": 15000
+    },
+    "Creon 6000": {
+        "lipase": 6000,
+        "protease": 19000,
+        "amylase": 30000
+    },
+    "Creon 12000": {
+        "lipase": 12000,
+        "protease": 38000,
+        "amylase": 60000
+    },
+    "Creon 24000": {
+        "lipase": 24000,
+        "protease": 76000,
+        "amylase": 120000
+    },
+    "Creon 36000": {
+        "lipase": 36000,
+        "protease": 114000,
+        "amylase": 180000
+    },
+    "Zenpep 3000": {
+        "lipase": 3000,
+        "protease": 10000,
+        "amylase": 16000
+    },
+    "Zenpep 5000": {
+        "lipase": 5000,
+        "protease": 17000,
+        "amylase": 27000
+    },
+    "Zenpep 10000": {
+        "lipase": 10000,
+        "protease": 34000,
+        "amylase": 55000
+    },
+    "Zenpep 15000": {
+        "lipase": 15000,
+        "protease": 51000,
+        "amylase": 82000
+    },
+    "Zenpep 20000": {
+        "lipase": 20000,
+        "protease": 68000,
+        "amylase": 109000
+    },
+    "Zenpep 25000": {
+        "lipase": 25000,
+        "protease": 85000,
+        "amylase": 136000
+    },
+    "Zenpep 40000": {
+        "lipase": 40000,
+        "protease": 136000,
+        "amylase": 218000
+    },
+}
